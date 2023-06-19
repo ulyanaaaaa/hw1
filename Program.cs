f52d110eb322f2858d11499556a174da8a922849
Ulyana <ulyanaharbuz@gmail.com>
2023-06-20 00:10:47 +0200

    added another excs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,47 @@
-﻿Console.Write("Enter number: ");
+﻿// Console.Write("Enter first number: ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+// if (firstNum > secondNum)
+// {
+//     Console.Write(firstNum+" > "+secondNum);
+// }
+// else
+// {
+//     Console.Write(secondNum+" > "+firstNum);
+// }
+
+
+
+// Console.Write("Enter first number: ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter third number: ");
+// int thirdNum = Convert.ToInt32(Console.ReadLine());
+// if (firstNum > secondNum && firstNum > thirdNum)
+//     {
+//         Console.Write("Biggest number is: "+firstNum);
+//     }
+// else if (secondNum > firstNum && secondNum > thirdNum)
+//     {
+//         Console.Write("Biggest number is:"+secondNum);
+//     }
+// else Console.Write("Biggest number is: "+thirdNum);
+
+
+
+// Console.Write("Enter number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 2 == 0)
+// {
+//     Console.Write("Even number");
+// }
+// else Console.Write("Not even number");
+
+
+
+Console.Write("Enter number: ");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 int zero = 0;
 if (userNumber > zero)
